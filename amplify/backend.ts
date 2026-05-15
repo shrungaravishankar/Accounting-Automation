af4f373a75fe5c90181277b9fb519cfaf29bed41
shrungaravishankar <shrunga.numa@gmail.com>
2026-05-15 17:32:40 +0530

    Update backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,6 +1,28 @@
 import { defineBackend } from '@aws-amplify/backend';
+import { PolicyStatement, Effect } from 'aws-cdk-lib/aws-iam';
 import { auth } from './auth/resource';
+import { data } from './data/resource';
+import { inviteUser } from './functions/invite-user/resource';
 
-defineBackend({
-  auth
+const backend = defineBackend({
+  auth,
+  data,
+  inviteUser
 });
+
+// Pass the User Pool ID to the Lambda as an env var.
+const userPool = backend.auth.resources.userPool;
+backend.inviteUser.addEnvironment('USER_POOL_ID', userPool.userPoolId);
+
+// Grant the Lambda permission to create users and add them to groups
+// inside *this* user pool only.
+backend.inviteUser.resources.lambda.addToRolePolicy(
+  new PolicyStatement({
+    effect: Effect.ALLOW,
+    actions: [
+      'cognito-idp:AdminCreateUser',
+      'cognito-idp:AdminAddUserToGroup'
+    ],
+    resources: [userPool.userPoolArn]
+  })
+);
